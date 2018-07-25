d26ef918a432bcfb5047b102e6e3d7c5a35a30e3
weige <18377618028@163.com>
2018-07-26 00:30:22 +0800

    6

--- application/admin/controller/TestController.php
+++ application/admin/controller/TestController.php
@@ -15,7 +15,7 @@ class TestController extends Controller
      * @return \think\Response
      */
      public function model(){
-         echo '111';
+         echo '1115';
      	echo md5("123456".config('password_salt')); die;
 //     	$catModel = new Category();
 //     	$data = ['cat_name'=>'溜溜球','pid'=>10];
